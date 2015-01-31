e7e0681e4155a713ebba78e5fdc06092a90e76e4
sedevc <info@devc.se>
2015-02-01 00:43:13 +0100

    Added pictures

--- btcvalidator.py
+++ btcvalidator.py
@@ -1,3 +1,4 @@
+
 from bitcoinaddress import validate
 from Tkinter import *
 
@@ -7,20 +8,31 @@ class MyController():
         self.model = MyModel(self)
         self.view = MyView(self)
         self.view.setEntry_text('Bitcoin address')
-        self.view.setLabel_text('Validate')
+        self.view.setLabel_text('Ready')
     def quitButtonPressed(self):
         self.parent.destroy()
     def validateButtonPressed(self):
-        self.view.setLabel_text(self.view.entry_text.get() + self.model.validateAddress(self.view.entry_text.get()))
-
-
-
+        #self.view.setLabel_text()
+        if validate(self.view.entry_text.get()):
+            self.view.setGreenImg()
+            self.view.setLabel_text(self.view.entry_text.get())
+        else:
+            self.view.setRedImg()
+            self.view.setLabel_text('Invalid address')
 class MyView(Frame):
     def loadView(self):
-        entry = Entry(self.frame, width = 45, textvariable = self.entry_text).grid(row = 0, column = 0, pady = 10, padx = 10, sticky = EW)
-        label = Label(self.frame,textvariable = self.label_text).grid(row = 1, column = 0, pady = 10, padx = 10, sticky = EW)
-        addButton = Button(self.frame,text = "Validate", command = self.vc.validateButtonPressed).grid(row = 0, column = 1)
-        quitButton = Button(self.frame,text = 'Quit', command= self.vc.quitButtonPressed).grid(row = 0,column = 2)
+        self.entry = Entry(self.frame, width = 45, textvariable = self.entry_text).grid(row = 0, column = 0, pady = 10, padx = 10, sticky = EW)
+        self.label = Label(self.frame,textvariable = self.label_text).grid(row = 1, column = 0, pady = 10, padx = 10, sticky = EW)
+        self.addButton = Button(self.frame,text = "Validate", command = self.vc.validateButtonPressed).grid(row = 0, column = 1)
+        self.quitButton = Button(self.frame,text = 'Quit', command= self.vc.quitButtonPressed).grid(row = 0, column = 2)
+        self.redimg = PhotoImage(file="red.gif")
+        self.greenimg = PhotoImage(file="green.gif")
+
+    def setGreenImg(self):
+        self.checkmark = Label(self.frame, image=self.greenimg).grid(row = 1, column = 2)
+    def setRedImg(self):
+        self.checkmark = Label(self.frame, image=self.redimg).grid(row = 1, column = 2)
+
     def __init__(self, vc):
         self.frame = Frame()
         self.frame.grid(row = 0,column=0)
@@ -50,10 +62,10 @@ def main():
     root = Tk()
     frame = Frame(root)
     root.title('Bitcoin address validator')
-    root.geometry("500x100")
+    root.geometry("500x120")
     app = MyController(root)
-    root.mainloop()  
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    main() 
+    main()
